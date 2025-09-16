f4f3f069227613a07c4b9020b4b7ba3de9b63c59
Bhargav Sai <bhargavsai7788@gmail.com>
2025-09-16 17:15:29 +0530

    comments in run.py

--- run.py
+++ run.py
@@ -4,3 +4,15 @@ app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+# lorem 23ipsum 45 
+# dolor sit amet, consectetur adipiscing elit. 
+# Sed do eiusmod tempor incididunt ut labore et dolore
+# magna aliqua. Ut enim ad minim veniam, quis nostrud e
+# xercitation ullamco laboris nisi ut aliquip ex ea commodo 
+# consequat. Duis aute irure dolor in reprehenderit in voluptate
+# velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+# lorem ipsum 34
+# sint occaecat cupidatat non proident, sunt in culpa qui officia
+# deserunt mollit anim id est laborum.
